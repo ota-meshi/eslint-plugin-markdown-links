206d1fd6c9cbbe4049ac6f576c161211b773f438
Amndeep Singh Mann <amann@mitre.org>
2026-04-09 22:09:24 -0400

    autogenned files
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- src/configs/recommended.ts
+++ src/configs/recommended.ts
@@ -1,24 +1,22 @@
 // IMPORTANT!
 // This file has been automatically generated,
 // in order to update its content execute "npm run update"
-import type { ESLint, Linter } from "eslint";
-import plugin from "../index.ts";
-import markdown from "@eslint/markdown";
-export const name = "markdown-links/recommended";
-export const files = ["*.md", "**/*.md"];
-export const language = "markdown/gfm";
-export const languageOptions = {
-  frontmatter: "yaml",
-};
-export const plugins = {
-  markdown: markdown as ESLint.Plugin,
-  // eslint-disable-next-line @typescript-eslint/naming-convention -- ignore
-  get "markdown-links"(): ESLint.Plugin {
-    return plugin;
-  },
-};
-export const rules: Linter.RulesRecord = {
+import type { Linter } from "eslint";
+import base from "./base.ts";
+
+const ruleset: Linter.RulesRecord = {
   // eslint-plugin-markdown-links rules
   "markdown-links/no-missing-path": "error",
   "markdown-links/no-self-destination": "error",
 };
+
+const config: Linter.Config[] = [
+  ...base,
+  {
+    rules: {
+      ...ruleset,
+    },
+  },
+];
+
+export default config;
